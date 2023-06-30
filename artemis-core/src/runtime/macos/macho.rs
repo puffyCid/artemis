47c21a6aa92498af4b74ffaa680ed06594d625d1
puffyCid <16283453+puffyCid@users.noreply.github.com>
2023-06-30 00:47:03 -0400

    test fix

--- artemis-core/src/runtime/macos/macho.rs
+++ artemis-core/src/runtime/macos/macho.rs
@@ -46,7 +46,7 @@ mod tests {
     #[test]
     fn test_get_macho() {
         let test = "Ly8gZGVuby1mbXQtaWdub3JlLWZpbGUKLy8gZGVuby1saW50LWlnbm9yZS1maWxlCi8vIFRoaXMgY29kZSB3YXMgYnVuZGxlZCB1c2luZyBgZGVubyBidW5kbGVgIGFuZCBpdCdzIG5vdCByZWNvbW1lbmRlZCB0byBlZGl0IGl0IG1hbnVhbGx5CgpmdW5jdGlvbiBnZXRfbWFjaG8ocGF0aCkgewogICAgY29uc3QgZGF0YSA9IERlbm9bRGVuby5pbnRlcm5hbF0uY29yZS5vcHMuZ2V0X21hY2hvKHBhdGgpOwogICAgaWYgKGRhdGEgPT09ICIiKSB7CiAgICAgICAgcmV0dXJuIG51bGw7CiAgICB9CiAgICBjb25zdCBtYWNobyA9IEpTT04ucGFyc2UoZGF0YSk7CiAgICByZXR1cm4gbWFjaG87Cn0KZnVuY3Rpb24gZ2V0TWFjaG8ocGF0aCkgewogICAgcmV0dXJuIGdldF9tYWNobyhwYXRoKTsKfQpmdW5jdGlvbiBtYWluKCkgewogICAgY29uc3QgYmluX3BhdGggPSAiL2JpbiI7CiAgICBjb25zdCBtYWNob3MgPSBbXTsKICAgIGZvciAoY29uc3QgZW50cnkgb2YgRGVuby5yZWFkRGlyU3luYyhiaW5fcGF0aCkpewogICAgICAgIGlmICghZW50cnkuaXNGaWxlKSB7CiAgICAgICAgICAgIGNvbnRpbnVlOwogICAgICAgIH0KICAgICAgICBjb25zdCBtYWNob19wYXRoID0gYCR7YmluX3BhdGh9LyR7ZW50cnkubmFtZX1gOwogICAgICAgIGNvbnN0IGluZm8gPSBnZXRNYWNobyhtYWNob19wYXRoKTsKICAgICAgICBpZiAoaW5mbyA9PT0gbnVsbCkgewogICAgICAgICAgICBjb250aW51ZTsKICAgICAgICB9CiAgICAgICAgY29uc3QgbWV0YSA9IHsKICAgICAgICAgICAgcGF0aDogbWFjaG9fcGF0aCwKICAgICAgICAgICAgbWFjaG86IGluZm8KICAgICAgICB9OwogICAgICAgIG1hY2hvcy5wdXNoKG1ldGEpOwogICAgfQogICAgcmV0dXJuIG1hY2hvczsKfQptYWluKCk7Cgo=";
-        let mut output = output_options("runtime_test", "nowhere", "./tmp", true);
+        let mut output = output_options("runtime_test", "local", "./tmp", true);
 
         let script = JSScript {
             name: String::from("bin_machos"),
