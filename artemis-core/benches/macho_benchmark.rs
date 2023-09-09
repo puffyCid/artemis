ff6ca76fc244e8e8e69e3654e02f488dc7b1da84
puffyCid <16283453+puffyCid@users.noreply.github.com>
2023-09-09 03:24:39 +0000

    Benchmarking ideas (#63)

---
+++ artemis-core/benches/macho_benchmark.rs
@@ -0,0 +1,21 @@
+use artemis_core::core::parse_toml_file;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::path::PathBuf;
+
+fn macho_files(path: &str) {
+    let _ = parse_toml_file(&path).unwrap();
+}
+
+fn bench_macho_files(c: &mut Criterion) {
+    let mut test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    test_path.push("tests/test_data/macos/benchmarks/macho_files.toml");
+
+    c.bench_function("Benching Macho parsing Filelisting", |b| {
+        b.iter(|| macho_files(&test_path.display().to_string()))
+    });
+}
+
+#[cfg(target_os = "macos")]
+criterion_group!(benches, bench_macho_files);
+#[cfg(target_os = "macos")]
+criterion_main!(benches);
