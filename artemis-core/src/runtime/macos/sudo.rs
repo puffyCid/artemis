80907d0fa2d9ac4072a3ff2b5a58479604ad3c44
puffyCid <16283453+puffyCid@users.noreply.github.com>
2024-02-07 21:06:11 -0500

    updates

--- artemis-core/src/runtime/macos/sudo.rs
+++ artemis-core/src/runtime/macos/sudo.rs
@@ -43,6 +43,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "Takes time to run"]
     fn test_get_sudologs() {
         let test = "Ly8gaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3B1ZmZ5Y2lkL2FydGVtaXMtYXBpL21hc3Rlci9zcmMvdW5peC9zdWRvbG9ncy50cwpmdW5jdGlvbiBnZXRNYWNvc1N1ZG9Mb2dzKCkgewogIGNvbnN0IGRhdGEgPSBEZW5vLmNvcmUub3BzLmdldF9zdWRvbG9ncygpOwogIGNvbnN0IGxvZ19kYXRhID0gSlNPTi5wYXJzZShkYXRhKTsKICByZXR1cm4gbG9nX2RhdGE7Cn0KCi8vIG1haW4udHMKZnVuY3Rpb24gbWFpbigpIHsKICBjb25zdCBkYXRhID0gZ2V0TWFjb3NTdWRvTG9ncygpOwogIHJldHVybiBkYXRhOwp9Cm1haW4oKTsK";
         let mut output = output_options("runtime_test", "local", "./tmp", false);
