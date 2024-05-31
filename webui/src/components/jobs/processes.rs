090f696e15552f45db35c67417254bbb16639fe2
puffyCid <16283453+puffyCid@users.noreply.github.com>
2024-05-30 22:53:04 -0400

    updates

--- webui/src/components/jobs/processes.rs
+++ webui/src/components/jobs/processes.rs
@@ -19,7 +19,27 @@ struct EndpointProcesses {
 /// Display process listing info
 pub(crate) fn EndpointProcesses(procs: Option<ProcessJob>) -> impl IntoView {
     if procs.is_none() {
-        return view! { <div>"No processes"</div> };
+        return view! {
+          <div class="m-4 flex items-center">
+            <button class="btn btn-sm btn-outline btn-primary">
+              "Refresh"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        };
     }
     let proc_data = procs.unwrap();
     let headers = vec!["Path", "Name", "PID", "PPID", "Start Time"];
@@ -145,7 +165,7 @@ fn SearchProcesses(
     let next_disabled = move || proc_get.get().count > info.get().unwrap_or_default().len() as i32;
 
     view! {
-      <div class="grid grid-cols-4 p-2 gap-2">
+      <div class="grid grid-cols-5 p-2 gap-2">
         <form on:submit=search_submit>
           <label class="input input-sm input-bordered flex items-center gap-2">
             <input type="text" class="grow" node_ref=search_form placeholder="Search Processes"/>
@@ -186,6 +206,23 @@ fn SearchProcesses(
                 .collect::<Vec<_>>()}
           </ul>
         </div>
+        <button class="btn btn-sm btn-outline btn-primary">
+          "Refresh"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+            ></path>
+          </svg>
+        </button>
         <button
           class="join-item btn btn-sm btn-outline"
           disabled=previous_disabled
