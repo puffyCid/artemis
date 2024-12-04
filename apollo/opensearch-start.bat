24463d730dca6fb75f75d3d91e0ee895be10b71d
puffycid <puffycyan@gmail.com>
2024-12-04 01:39:33 -0500

    fixes

--- apollo/opensearch-start.bat
+++ apollo/opensearch-start.bat
@@ -1,3 +1,3 @@
 @echo off
 
-docker.exe run -it -p 9200:9200 -p 9600:9600 -e OPENSEARCH_INITIAL_ADMIN_PASSWORD="Ughsocomplex123567890!" -e "discovery.type=single-node"  --name opensearch-node -d opensearchproject/opensearch:latest
+podman.exe run -it -p 9200:9200 -p 9600:9600 -e OPENSEARCH_INITIAL_ADMIN_PASSWORD="Ughsocomplex123567890!" -e "discovery.type=single-node"  --name opensearch-node -d opensearchproject/opensearch:latest
