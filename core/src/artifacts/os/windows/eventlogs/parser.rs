61bbb51a0bc3fe23c3e55816d78df5d5ec59c09a
puffycid <puffycyan@gmail.com>
2024-12-25 22:23:40 -0500

    updates

--- core/src/artifacts/os/windows/eventlogs/parser.rs
+++ core/src/artifacts/os/windows/eventlogs/parser.rs
@@ -578,7 +578,7 @@ mod tests {
         test_location.push("tests/test_data/windows/eventlogs");
         let mut output = output_options("eventlog_temp", "local", "./tmp", false);
 
-        let test = json!({"key": "value"});
-        output_logs(&Ok(test), &mut output, &false, &0, "testing", &true).unwrap();
+        let mut test = json!({"key": "value"});
+        output_logs(&Ok(&mut test), &mut output, &false, &0, "testing", &true).unwrap();
     }
 }
