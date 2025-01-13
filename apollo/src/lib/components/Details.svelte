825b8794ead80b82d5a2448fe21b0af938affb3f
puffycid <puffycyan@gmail.com>
2025-01-13 00:29:38 -0500

    apollo updates

--- apollo/src/lib/components/Details.svelte
+++ apollo/src/lib/components/Details.svelte
@@ -4,11 +4,20 @@
     export let data: TimelineEntry;
 
     let visible = false;
+    let modalOpen = false;
+
     /**
      * Toggle timeline details
      */
     function viewData() {
         visible = !visible;
+    }
+
+    /**
+     * Toggle the modal for nested objects
+     */
+    function toggleModal() {
+        modalOpen = !modalOpen;
     }
 </script>
 
@@ -33,7 +42,49 @@
                     {#each Object.entries(data) as [key, value]}
                         <tr>
                             <td>{key}</td>
-                            <td>{value}</td>
+                            {#if Array.isArray(value) && typeof value.at(0) != "object"}
+                                <td>
+                                    {#each value as entry}
+                                        <div class="badge badge-outline">
+                                            {entry}
+                                        </div>
+                                    {/each}
+                                </td>
+                                <!--If we have nested objects or array of objects. Use modal to display-->
+                            {:else if typeof value === "object"}
+                                <td>
+                                    <button
+                                        class="btn btn-xs btn-outline"
+                                        on:click={toggleModal}
+                                        >Details
+                                    </button>
+                                    <dialog
+                                        class="modal"
+                                        class:modal-open={modalOpen}
+                                    >
+                                        <div class="modal-box">
+                                            <h3 class="text-lg font-bold">
+                                                Additional Details
+                                            </h3>
+                                            <form
+                                                method="dialog"
+                                                class="modal-action"
+                                            >
+                                                <button
+                                                    class="btn btn-xs btn-circle btn-ghost absolute right-2 top-2"
+                                                    on:click={toggleModal}
+                                                    >X</button
+                                                >
+                                            </form>
+                                            <pre>
+                                                {JSON.stringify(value, null, 2)}
+                                            </pre>
+                                        </div>
+                                    </dialog>
+                                </td>
+                            {:else}
+                                <td>{value}</td>
+                            {/if}
                         </tr>
                     {/each}
                 </tbody>
--- apollo/src/lib/components/Menu.svelte
+++ apollo/src/lib/components/Menu.svelte
@@ -1,24 +1,4 @@
 <script lang="ts">
-    import { listArtifacts } from "$lib/queries/artifacts";
-    import { isError } from "$lib/queries/error";
-    let artifacts: string[] = [];
-
-    let index = "test";
-
-    /**
-     * List of artifacts
-     */
-    async function getList() {
-        const status = await listArtifacts(index);
-        if (isError(status)) {
-            return;
-        }
-
-        // Loop and get each artifact name
-        for (const entry of status.aggregations.artifacts.buckets) {
-            artifacts.push(entry.key);
-        }
-    }
 </script>
 
 <div class="col-span-full">
@@ -27,22 +7,6 @@
         <div class="navbar-center hidden lg:flex">
             <ul class="menu menu-horizontal px-1">
                 <li><a href="/timeline">Timeline</a></li>
-                <li>
-                    <details>
-                        <summary>Artifacts</summary>
-                        <ul class="bg-base-100 rounded-t-none p-2">
-                            {#await getList()}
-                                <li>Loading...</li>
-                            {:then}
-                                {#each artifacts as entry}
-                                    <li class="text-black"><a>{entry}</a></li>
-                                {/each}
-                            {:catch error}
-                                <li>Query failed: {error}</li>
-                            {/await}
-                        </ul>
-                    </details>
-                </li>
                 <li><a>Settings</a></li>
                 <li><a href="/about">About</a></li>
             </ul>
