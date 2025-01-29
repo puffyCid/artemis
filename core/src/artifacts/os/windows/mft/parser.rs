1d391735763aca47047625d076dce2af0f848f9e
puffycid <puffycyan@gmail.com>
2025-01-29 01:13:06 -0500

    mft improvements

--- core/src/artifacts/os/windows/mft/parser.rs
+++ core/src/artifacts/os/windows/mft/parser.rs
@@ -16,17 +16,10 @@ use crate::{
  * Only NTFS 3.1 or higher supported
  * TODO:
  *
- * 1. Check for recursive parent mfts. Cache should stop that?
- *    - Check for recursive attribute list
- * 2. Do not include base_extensions (ATTRIBUTE_LIST) entries in the final output. Instead combine them with base_entries
- *    1. Requires that we parse the MFT twice? :/
- *    2. First parse MFT and only grab the base_extensions. Cache them.
- *    3. Next parse the MFT and only grab the base_entries.
- *    4. Combine the base_extensions with the base_entry. Via index and sequence matching?
- *    5. Once combined you have all of your attributes
  * 3. Remove panics
  * 4. update file_attribute_flags function in attributes.rs
  * 5. Test on MFT files in artifact musuem and windows
+ * 6. Make sure .unwraps() are gone or checked
  */
 
 /// Try create a filelisting from provided MFT file
