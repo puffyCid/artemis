54447fbafe847f0ab0bc44692af53e46785f34a4
puffycid <puffycyan@gmail.com>
2025-05-13 20:37:11 -0400

    removed apollo idea

--- apollo/src/routes/about/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { About } from "$lib/types/about";
-import type { ErrorStatus } from "$lib/types/search";
-import { invoke } from "@tauri-apps/api/core";
-
-export const load = async (): Promise<About | ErrorStatus> => {
-    return await invoke("about_me");
-};
