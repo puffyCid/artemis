b49e07df492400d5218c4826a81586c359a8549e
puffyCid <16283453+puffyCid@users.noreply.github.com>
2025-12-28 16:43:35 -0500

    loginitems fixes

--- forensics/src/artifacts/os/macos/loginitems/loginitem.rs
+++ forensics/src/artifacts/os/macos/loginitems/loginitem.rs
@@ -1,74 +1,25 @@
 use super::error::LoginItemError;
 use super::plist::get_bookmarks;
 use crate::artifacts::os::macos::plist::property_list::parse_plist_file_dict;
-use crate::{
-    artifacts::os::macos::bookmarks::parser::parse_bookmark, filesystem::files::list_files,
-};
+use crate::filesystem::files::list_files;
 use common::macos::LoginItemsData;
 use log::{error, warn};
 
 /// Parse User `LoginItems` from provided path
 pub(crate) fn parse_loginitems(path: &str) -> Result<Vec<LoginItemsData>, LoginItemError> {
-    // Parse PLIST file and get any bookmark data
-    let loginitems_results = get_bookmarks(path);
-    let loginitems_data = match loginitems_results {
+    // Parse PLIST file and get any loginitems data
+    let items = match get_bookmarks(path) {
         Ok(data) => data,
         Err(err) => {
             error!("[loginitem] Failed to read plist {path}: {err:?}");
             return Err(LoginItemError::Plist);
         }
     };
-    if loginitems_data.is_empty() {
-        return Ok(Vec::new());
-    }
 
-    let mut loginitems_results: Vec<LoginItemsData> = Vec::new();
-    for data in loginitems_data {
-        let results = parse_bookmark(&data);
-        let bookmark = match results {
-            Ok(bookmark_data) => bookmark_data,
-            Err(err) => {
-                // Even if we fail to parse one (1) entry keep trying the others
-                // Plist may contain non-bookmark binary data
-                error!("Failed to parse bookmark data: {err:?}");
-                continue;
-            }
-        };
-        let mut loginitem_data = LoginItemsData {
-            path: bookmark.path,
-            cnid_path: bookmark.cnid_path,
-            created: bookmark.created,
-            volume_path: bookmark.volume_path,
-            volume_url: bookmark.volume_url,
-            volume_name: bookmark.volume_name,
-            volume_uuid: bookmark.volume_uuid,
-            volume_size: bookmark.volume_size,
-            volume_created: bookmark.volume_created,
-            volume_flags: bookmark.volume_flags,
-            volume_root: bookmark.volume_root,
-            localized_name: bookmark.localized_name,
-            security_extension_rw: bookmark.security_extension_rw,
-            security_extension_ro: bookmark.security_extension_ro,
-            target_flags: bookmark.target_flags,
-            username: bookmark.username,
-            folder_index: bookmark.folder_index,
-            uid: bookmark.uid,
-            creation_options: bookmark.creation_options,
-            file_ref_flag: bookmark.file_ref_flag,
-            is_bundled: false,
-            app_id: String::new(),
-            app_binary: String::new(),
-            is_executable: bookmark.is_executable,
-            source_path: String::new(),
-        };
-        loginitem_data.source_path = path.to_string();
-        loginitems_results.push(loginitem_data);
-    }
-
-    Ok(loginitems_results)
+    Ok(items)
 }
 
-/// Parse `LoginItems` associated with bundled apps
+/// Parse `LoginItems` associated with bundled apps. Legacy version
 pub(crate) fn loginitems_bundled_apps_path(
     path: &str,
 ) -> Result<Vec<LoginItemsData>, LoginItemError> {
@@ -143,7 +94,7 @@ pub(crate) fn loginitems_bundled_apps_path(
     Ok(loginitems_vec)
 }
 
-/// Get `LoginItems` data from embedded `LoginItems` in Apps
+/// Get `LoginItems` data from historical embedded `LoginItems` in Apps
 pub(crate) fn loginitem_apps_system() -> Result<Vec<LoginItemsData>, LoginItemError> {
     let default_path = "/var/db/com.apple.xpc.launchd/";
     loginitems_bundled_apps_path(default_path)
