4fcf5f844e5a5e16800f4e347c8f21c727171ba3
puffycid <puffycyan@gmail.com>
2026-04-25 01:57:42 -0400

    fix

--- forensics/src/artifacts/os/linux/sudo/logs.rs
+++ forensics/src/artifacts/os/linux/sudo/logs.rs
@@ -14,7 +14,7 @@ pub(crate) fn grab_sudo_logs(options: &LinuxSudoOptions) -> Result<Vec<Journal>,
         vec![alt_dir.clone()]
     } else {
         let persist = "/var/log/journal/";
-        let tmp = "/run/systemd/journal";
+        let tmp = "/run/log/journal";
         let mut logs = list_files_directories(persist).unwrap_or_default();
         let mut tmp_files = list_files_directories(tmp).unwrap_or_default();
 
