29b3be7af4ec6ea978cdef94602879d0934a5c98
puffyCid <puffycyan@gmail.com>
2026-05-02 14:30:09 -0400

    fixing flakey tests

--- forensics/tests/tasks_tester.rs
+++ forensics/tests/tasks_tester.rs
@@ -46,8 +46,10 @@ fn validate_output(output: &PathBuf) {
         let value = line.unwrap();
         let info: TaskInfo = serde_json::from_str(&value).unwrap();
         assert!(!info.name.is_empty());
-        if !info.action.contains("VSIXConfigurationUpdater") && !info.path.contains("S-1-5-21-") {
-            println!("{info:?}");
+        if !info.action.contains("VSIXConfigurationUpdater")
+            && !info.path.contains("S-1-5-21-")
+            && !info.action.contains("hosted-compute-agent")
+        {
             assert!(!info.registry_tree_path.is_empty());
             assert!(!info.id.is_empty());
         }
