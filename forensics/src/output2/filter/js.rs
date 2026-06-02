e004848d0c6480699eb24c1f4ffbaf3d3edb4d76
puffyCid <puffycyan@gmail.com>
2026-06-02 01:09:36 -0400

    updates

--- forensics/src/output2/filter/js.rs
+++ forensics/src/output2/filter/js.rs
@@ -136,7 +136,7 @@ mod tests {
         let config = OutputConfig::default();
         let context = CollectionContext::new(&config, PathBuf::from("./tmp"));
         let mut first = Map::new();
-        first.insert("path".to_string(), "/tmp/one.txt".into());
+        first.insert("path".to_string(), "/tmp/two.txt".into());
         first.insert("size".to_string(), 1235.into());
         let mut records = SingleRecordStream::new(Record::Json(JsonRecord::new(first)));
         let js = JsFilterRecordStream::new(&mut records, "YXN5bmMgZnVuY3Rpb24gbWFpbihyZWNvcmQsIGNvbnRleHQpIHsKICBhd2FpdCBQcm9taXNlLnJlc29sdmUoKTsKICBpZihyZWNvcmQucGF0aCAhPT0gIi90bXAvdHdvLnR4dCIpIHsKICAgIHJldHVybiBudWxsOwogIH0KIGNvbnNvbGUubG9nKGBJIGdvdCAke3JlY29yZC5wYXRofWApOwogIGNvbnNvbGUubG9nKGBDb250ZXh0IGlzIGVuZHBvaW50IElEOiAke2NvbnRleHQuZW5kcG9pbnRfaWR9YCk7CiAgcmVjb3JkWyJtZXNzYWdlIl0gPSAiWW91IGdvdCBhc3luYyBmaWx0ZXJlZCEiOwogIHJlY29yZFsiZmlsdGVyZWRfYnkiXSA9IGNvbnRleHQuZmlsdGVyX25hbWU7CiAgcmVjb3JkWyJhc3luY19maWx0ZXIiXSA9IHRydWU7CiAgcmV0dXJuIHJlY29yZDsKfQ==", "test", "test", &context).unwrap();
