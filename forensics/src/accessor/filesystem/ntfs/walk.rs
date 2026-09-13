5d8f74fc16905863aafe970ae07f7e334ee730db
puffyCid <puffycyan@gmail.com>
2026-09-12 23:01:08 -0400

    updates

--- forensics/src/accessor/filesystem/ntfs/walk.rs
+++ forensics/src/accessor/filesystem/ntfs/walk.rs
@@ -89,7 +89,7 @@ fn process_child_entries<T: Read + Seek>(
             | EntryKind::Symlink
             | EntryKind::CharDevice => 0,
             // Only files have sizes
-            EntryKind::File => read_file_size(file, reader, child.file_ref.file_record_number)?,
+            EntryKind::File => read_file_size(file, reader)?,
         };
 
         let scheme_path = format!("ntfs:{}", child.display_path);
@@ -283,14 +283,10 @@ pub(crate) fn get_file_size<T: Read + Seek>(
 ) -> AccessorResult<u64> {
     // Get direct access to the file via file reference
     let file = ntfs.file(reader, record_number).map_err(ntfs_err)?;
-    read_file_size(file, reader, record_number)
+    read_file_size(file, reader)
 }
 
-fn read_file_size<T: Read + Seek>(
-    file: NtfsFile<'_>,
-    reader: &mut T,
-    record_number: u64,
-) -> AccessorResult<u64> {
+fn read_file_size<T: Read + Seek>(file: NtfsFile<'_>, reader: &mut T) -> AccessorResult<u64> {
     match file.data(reader, "") {
         Some(Ok(item)) => Ok(item.to_attribute().map_err(ntfs_err)?.value_length()),
         Some(Err(err)) => Err(ntfs_err(err)),
