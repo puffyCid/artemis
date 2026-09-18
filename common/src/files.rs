ba9f55b81dd43e5ac4ea5d044681d93295c3a8ef
puffyCid <puffycyan@gmail.com>
2026-09-17 21:29:05 -0400

    updates

--- common/src/files.rs
+++ common/src/files.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+use crate::windows::{ADSInfo, CompressionType, Namespace};
+
 #[derive(Deserialize)]
 /// Supported hashes
 pub struct Hashes {
@@ -136,4 +138,43 @@ pub enum Attributes {
     SetGid,
     /// Unix
     Sticky,
+}
+
+#[derive(Debug, Serialize, Default)]
+pub struct FileNtfsInfo {
+    pub full_path: String,
+    pub directory: String,
+    pub filename: String,
+    pub extension: String,
+    pub created: String,
+    pub modified: String,
+    pub changed: String,
+    pub accessed: String,
+    pub filename_created: String,
+    pub filename_modified: String,
+    pub filename_changed: String,
+    pub filename_accessed: String,
+    pub attributes: Vec<Attributes>,
+    pub size: u64,
+    pub md5: String,
+    pub sha1: String,
+    pub sha256: String,
+    pub kind: EntryKind,
+    pub depth: usize,
+    pub yara_hits: Vec<String>,
+    pub binary_info: Value,
+    pub display_path: String,
+    pub compressed_size: u64,
+    pub compression_type: CompressionType,
+    pub inode: u64,
+    pub sequence_number: u16,
+    pub parent_mft_reference: u64,
+    pub owner: u32,
+    pub namespace: Namespace,
+    pub ads_info: Vec<ADSInfo>,
+    pub usn: u64,
+    pub sid: u32,
+    pub user_sid: String,
+    pub group_sid: String,
+    pub drive: String,
 }
